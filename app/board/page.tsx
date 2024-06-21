6257a7922c06dbb74ae509adc936f3722a8ef01e
Takashi-Matsumura <mats@coral.plala.or.jp>
2024-06-21 20:54:48 +0900

    weekly layout

---
+++ app/board/page.tsx
@@ -0,0 +1,64 @@
+import Link from "next/link";
+import {
+  FaCircleChevronLeft,
+  FaCircleChevronRight,
+  FaHouse,
+  FaRegSquarePlus,
+} from "react-icons/fa6";
+
+export default function BoardPage() {
+  return (
+    <div className="container mx-auto">
+      <div className="flex flex-col items-center">
+        <div className="flex text-3xl w-full justify-between mt-10 mb-5">
+          <FaCircleChevronLeft />
+          2024年○月
+          <FaCircleChevronRight />
+        </div>
+        <div className="flex w-full h-[80vh]">
+          <div className="border-2 border-b-green-500 w-full text-center">
+            <p className="font-bold text-xl">月</p>
+            <p>○日</p>
+            <div className="flex flex-col border-4 rouded-md text-left p-2">
+              <p>時間</p>
+              <p>タイトル</p>
+              <p>内容</p>
+            </div>
+          </div>
+          <div className="border-2 border-b-green-500 w-full text-center">
+            <p className="font-bold text-xl">火</p>
+            <p>○日</p>
+          </div>
+          <div className="border-2 border-b-green-500 w-full text-center">
+            <p className="font-bold text-xl">水</p>
+            <p>○日</p>
+          </div>
+          <div className="border-2 border-b-green-500 w-full text-center">
+            <p className="font-bold text-xl">木</p>
+            <p>○日</p>
+          </div>
+          <div className="border-2 border-b-green-500 w-full text-center">
+            <p className="font-bold text-xl">金</p>
+            <p>○日</p>
+          </div>
+          <div className="border-2 border-b-green-500 w-full text-center">
+            <p className="font-bold text-xl text-blue-700">土</p>
+            <p>○日</p>
+          </div>
+          <div className="border-2 border-b-green-500 w-full text-center">
+            <p className="font-bold text-xl text-red-700">日</p>
+            <p>○日</p>
+          </div>
+        </div>
+        <div className="flex text-3xl w-full justify-between mt-5">
+          <Link href="/create">
+            <FaRegSquarePlus />
+          </Link>
+          <Link href="/">
+            <FaHouse />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+export default function HomePage() {
   return (
     <div className="container mx-auto">
       <div className="flex flex-col h-screen items-center justify-center">
         <h1 className="font-bold text-5xl">Hello World</h1>
+        <Link
+          href="/board"
+          className="bg-black text-white border-2 rounded-full px-4 py-2 mt-10"
+        >
+          click me
+        </Link>
       </div>
     </div>
   );
