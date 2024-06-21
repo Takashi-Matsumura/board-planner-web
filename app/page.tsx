f475941a2b2f393addc547fde2ca9508dc2437c0
Takashi-Matsumura <mats@coral.plala.or.jp>
2024-06-21 19:29:51 +0900

    1st commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="container mx-auto">
+      <div className="flex flex-col h-screen items-center justify-center">
+        <h1 className="font-bold text-5xl">Hello World</h1>
+      </div>
+    </div>
+  );
+}
