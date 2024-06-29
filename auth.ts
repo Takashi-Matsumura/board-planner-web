7a3750782e1c132c39c7c5476c77b0290c3c4457
Takashi-Matsumura <mats@coral.plala.or.jp>
2024-06-29 10:47:40 +0900

    add google oauth

--- auth.ts
+++ auth.ts
@@ -1,11 +1,15 @@
 import NextAuth, { NextAuthConfig } from "next-auth";
 import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export const config: NextAuthConfig = {
     theme: {
         logo: "https://next-auth.js.org/img/logo/logo-sm.png",
     },
-    providers: [GitHub({clientId: process.env.AUTH_GITHUB_ID, clientSecret: process.env.AUTH_GITHUB_SECRET})],
+    providers: [
+        GitHub({clientId: process.env.AUTH_GITHUB_ID, clientSecret: process.env.AUTH_GITHUB_SECRET}),
+        Google({clientId: process.env.AUTH_GOOGLE_ID, clientSecret: process.env.AUTH_GOOGLE_SECRET}),
+    ],
     basePath: "/api/auth",
     callbacks: {
         authorized({request, auth}) {
